adc699c3b1fa34f1a6689f165b00c284f8c0c4f5
wsmnnmm <1832004940@qq.com>
2021-12-31 17:01:31 +0800

    添加了prototype

--- src/jquery.js
+++ src/jquery.js
@@ -5,53 +5,71 @@ window.$ = window.jQuery = function (selectorOrArray) {
     } else if (selectorOrArray instanceof Array) {
         elements = selectorOrArray
     }
-    //api 可以操作elements
-    return {
-        find(selector) {
-            let array = []
-            for (let i = 0; i < elements.length; i++) {
-                const elements2 = Array.from(elements[i].querySelectorAll(selector))
-                array = array.concat(elements2)
-                //querySelectorAll查找的是伪数组
-            }
-            array.oldApi = this //this 就是 api
-            return jQuery(array)
-        },
-        each(fn) {
-            for (let i = 0; i < elements.length; i++) {
-                fn.call(null, elements[i], i)
-            }
-            return this
-        },
-        parent() {
-            const array = []
-            this.each((node) => {
-                if (array.indexOf(node.parentNode) === -1) {
-                    array.push(node.parentNode)
-                }
-            })
-            return jQuery(array)
-        },
-        children() {
-            const array = []
-            this.each((node) => {
-                array.push(...node.children)
-            })
-            return jQuery(array)
-        },
-        print() {
-            console.log(elements) //parent()调用jQuery的形参传进来的elements
-        },
-        addClass(className) {
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
-            }
-            return this
-        },
-        oldApi: selectorOrArray.oldApi,
-        end() {
-            return this.oldApi
-        },
+
+    function createElement(string) {
+        const container = document.createElement("template")
+        container.innerHTML = string.trim()
+        return container.content.firstChild
     }
+    // api 可以操作elements
+    const api = Object.create(jQuery.prototype)//创建一个对象，这个对象的__proto__为括号里面的东西
+    // 等价于
+    // const api = {__proto__: jQuery.prototype}
+    Object.assign(api, {
+        elements: elements,
+        oldApi: selectorOrArray.oldApi
+    })
+    // 等价于
+    // api.elements = elements
+    // api.oldApi = selectorOrArray.oldApi
+    return api
 }
 
+jQuery.prototype = {
+    constructor: jQuery,
+    find(selector) {
+        1
+        let array = []
+        for (let i = 0; i < elements.length; i++) {
+            const elements2 = Array.from(elements[i].querySelectorAll(selector))
+            array = array.concat(elements2)
+            //querySelectorAll查找的是伪数组
+        }
+        array.oldApi = this //this 就是 api
+        return jQuery(array)
+    },
+    each(fn) {
+        for (let i = 0; i < elements.length; i++) {
+            fn.call(null, elements[i], i)
+        }
+        return this
+    },
+    parent() {
+        const array = []
+        this.each((node) => {
+            if (array.indexOf(node.parentNode) === -1) {
+                array.push(node.parentNode)
+            }
+        })
+        return jQuery(array)
+    },
+    children() {
+        const array = []
+        this.each((node) => {
+            array.push(...node.children)
+        })
+        return jQuery(array)
+    },
+    print() {
+        console.log(elements) //parent()调用jQuery的形参传进来的elements
+    },
+    addClass(className) {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].classList.add(className)
+        }
+        return this
+    },
+    end() {
+        return this.oldApi
+    },
+}
